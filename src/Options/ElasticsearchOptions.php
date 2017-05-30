a6a68313800fa7217df40e129403b0bc259f8c55
Erik Norgren <erik.norgren@interactivesolutions.se>
2017-05-30 11:06:26 +0200

    Added additional elastic search options and removed hardcodes elastic search ip.

--- src/Options/ElasticsearchOptions.php
+++ src/Options/ElasticsearchOptions.php
@@ -25,6 +25,16 @@ final class ElasticsearchOptions extends AbstractOptions
     /**
      * @var string
      */
+    protected $username;
+
+    /**
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * @var string
+     */
     protected $prefix;
 
     /**
@@ -57,6 +67,38 @@ final class ElasticsearchOptions extends AbstractOptions
     public function setPort(int $port)
     {
         $this->port = $port;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername(string $username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
     }
 
     /**
